c3f61e043f4fd605e405a93c139f3dc4cce363d2
p1ratrulezzz <git@p1ratrulezzz.me>
2020-02-28 16:45:18 +0300

    HTML page

--- create_html.py
+++ create_html.py
@@ -11,14 +11,44 @@ def RenderIndexHtml(model):
     output.close()
 
 # Prepare model data
+page = {}
 mdfile = open("README.md", "r")
 
 html = markdown.markdown(mdfile.read())
-doc = minidom.parseString(html)
-
-page = {}
-page['content'] = html
 mdfile.close()
+
+doc = minidom.parseString('<html>' + html + '</html>')
+
+headerEl = doc.getElementsByTagName('h1')[0]
+page["title"] = headerEl.childNodes[0].data
+headerEl.parentNode.removeChild(headerEl)
+
+words = []
+
+for pEl in doc.documentElement.childNodes:
+    if (pEl.nodeType != pEl.ELEMENT_NODE):
+        continue
+
+    text = []
+    for strongEl in pEl.getElementsByTagName('strong'):
+        text.append(strongEl.firstChild.data)
+
+    words.append({
+        "text": " ".join(text).capitalize(),
+        "element": pEl,
+    })
+
+
+def sort_words(v):
+    return str(v['text']).lower()
+
+words.sort(key=sort_words)
+
+page["content"] = "\n"
+
+for word in words:
+    page["content"] += word['element'].toxml() + "\n"
+
 
 # Parse html and sort nodes alphabetically
 # Remove the header
